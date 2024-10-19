32614d43a18fce192e4568523995d37a45915aa6
David Smythe <davidwsmythe22@gmail.com>
2024-10-19 16:25:49 -0400

    Initial Commit

---
+++ Toggl-Part2/Toggl_Part2App.swift
@@ -0,0 +1,17 @@
+//
+//  Toggl_Part2App.swift
+//  Toggl-Part2
+//
+//  Created by David Smythe on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Toggl_Part2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

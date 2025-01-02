8c66acd4e5eee2326203ac2b693f3a1860497ad5
David Smythe <davidwsmythe22@gmail.com>
2025-01-02 00:55:04 +0000

    added data

---
+++ Toggl-Part2/Core/DataManager/ReturnAppManager.swift
@@ -0,0 +1,8 @@
+//
+//  returnAppManager.swift
+//  Toggl-Part2
+//
+//  Created by David Smythe on 12/23/24.
+//
+
+import Foundation
---
+++ Toggl-Part2/Core/StatsView/StatsManager.swift
@@ -0,0 +1,8 @@
+//
+//  StatsManager.swift
+//  Toggl-Part2
+//
+//  Created by David Smythe on 1/1/25.
+//
+
+import Foundation
